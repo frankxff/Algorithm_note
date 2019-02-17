c3a5d6b03f2b0b067784be23218257214ff9df71
frankxff <1227291519@qq.com>
2019-02-17 13:59:58 +0800

    bool:非零为true(1),零为false(0)

---
+++ chapter2/math_round.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+
+int main()
+{
+	double a = round(3.40);
+	double b = round(3.50);
+	double c = round(3.60);
+	printf("%d,%d,%d\n", int(a), int(b), int(c));
+	
+	return 0; 
+}
